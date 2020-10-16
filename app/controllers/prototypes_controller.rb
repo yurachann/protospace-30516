47116ef03cd86b6ed7321b6ba5d0942eb3f62c20
yurachann <motomu1220@gmail.com>
2020-10-16 12:40:35 +0900

    プロトタイプコントローラーの変数修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,7 +32,7 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to root_path
     else
       render :edit
